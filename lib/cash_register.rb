50d067455f9127032a54a21c802f5be7c9d28290
Nick Nobile <nickcnobile@gmail.com>
2020-10-03 22:41:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   end
 
   def apply_discount
-    @total = (@total - @total * @discount.to_f / 100).round(2)
+    @total = (@total - @total * @discount.to_f / 100)
     if @discount == 0
       "There is no discount to apply."
     else "After the discount, the total comes to $#{@total}."
