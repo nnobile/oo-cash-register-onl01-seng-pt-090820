90d7e24a1f6b4c0b3936246903b71c43b6d3756e
Nick Nobile <nickcnobile@gmail.com>
2020-10-03 21:47:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,9 @@
 class CashRegister
   attr_accessor :cash_register, :cash_register_with_discount, :total
 
-def initialize
-  @total = 0
-end
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    end
 
 end
