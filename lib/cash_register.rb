3be2568bb8e0f527ddccfb4977c14e85a46d9c24
Nick Nobile <nickcnobile@gmail.com>
2020-10-03 21:27:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,9 +3,11 @@ class CashRegister
 
 def initialize
   @total = 0
+  if employee_discount = true
+    return employee_discount
 end
 
 def self.cash_register_with_discount
-  discount = @total * .8
-  return total
+  emplyee_discount = @total * .8
+  return employee_discount
 end
